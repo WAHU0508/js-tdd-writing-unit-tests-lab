0418c7f9566bcc69bca7d39867585d76c61500fc
WAHU0508 <sharongikenye@gmail.com>
2024-08-07 15:15:50 +0300

    isPalindrome function and tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,34 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+    it("Should return true for words that are a combination of uppercase and lowercase letters", () => {
+        let word = "mom";
+        let palindrome = isPalindrome(word);
+        expect(palindrome).toBe(true);
+    });
+
+    it("Should return false for an empty string", () => {
+        let word = "";
+        let palindrome = isPalindrome(word);
+        expect(palindrome).toBe(false);
+    });
+
+    it("Should throw an error if input has any non-alphabetic characters", () => {
+        let word = "!!!";
+        expect(() => isPalindrome(word)).toThrow('Input must contain only alphabetic characters');
+    });
+    it("Should throw an error if input is not a string", () => {
+        let nonStringInput = 12345;
+        expect(() => isPalindrome(nonStringInput)).toThrow('Input must be a string');
+    });
+
+    it("Should throw an error if input is an array", () => {
+        let nonStringInput = ["mom"];
+        expect(() => isPalindrome(nonStringInput)).toThrow('Input must be a string');
+    });
+
+    it("Should throw an error if input is an object", () => {
+        let nonStringInput = { word: "mom" };
+        expect(() => isPalindrome(nonStringInput)).toThrow('Input must be a string');
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
-// Your code here
+export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+        throw new Error("Input must be a string");
+    }
+    if (!word) {
+        return false;
+    }
+    if (!/^[a-zA-Z]+$/.test(word)) {
+        throw new Error("Input must contain only alphabetic characters");
+    }
+    let reversal = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversal += word[i];
+    }
+    return word.toLowerCase() === reversal.toLowerCase();
+}
